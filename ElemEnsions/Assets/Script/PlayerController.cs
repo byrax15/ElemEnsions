bcd53135726e56be637f0b3e69189b4a115ff862
dovakin7 <xavier7.lemieux@hotmail.com>
2022-05-13 23:49:05 -0400

    playerControllerBase

--- ElemEnsions/Assets/Script/PlayerController.cs
+++ ElemEnsions/Assets/Script/PlayerController.cs
@@ -1,18 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private CharacterController cr;
+    public float speed;
+
+    public float gravity = -9.8f;
+    public float rotationSpeed = 0.8f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
+
+    private bool isGrounded = true;
+
+    private Vector3 velocity;
+    private Vector3 movement;
+
+    [SerializeField] private Transform cameraTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // downward acceleration
+        if (isGrounded &&
+            velocity.y < -2) //We only reset the velocity if we were falling and before we were grounded
+        {
+            velocity.y = -2f;
+        }
+        else
+        {
+            velocity.y += gravity;
+        }
+        Vector3 move = movement.x * cameraTransform.right.normalized + movement.z * cameraTransform.forward.normalized;
+        move.y = 0.0f;
+        cr.Move((velocity + move) * Time.deltaTime);
+
+        Quaternion rotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+    }
+
+    public void OnApplicationFocus(bool focus)
+    {
+        if(focus)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState= CursorLockMode.None;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Vector2 input = context.ReadValue<Vector2>();
         
+            movement = speed * new Vector3(input.x, 0.0f, input.y).normalized;
+            Debug.Log(movement);
+        }
+        if(context.canceled)
+            movement = Vector3.zero;
     }
 }
