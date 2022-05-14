384ef8419ca8f09529a5e93ed893b938669197c6
Xavier Lemieux <xavier7.lemieux@hotmail.com>
2022-05-14 18:44:34 -0400

    power par univers

--- ElemEnsions/Assets/Script/ToggleWaterCollision.cs
+++ ElemEnsions/Assets/Script/ToggleWaterCollision.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using Script;
+using System;
 
 public class ToggleWaterCollision : MonoBehaviour
 {
+    [SerializeField]private DimensionChangeMediator mediator;
     private void Start()
     {
+        mediator.AddListener(DimensionChange);
         ToggleWater(false);
+    }
+
+    private void DimensionChange(Dimension oldDim, Dimension newDim)
+    {
+        ToggleWater(newDim == Dimension.Water);
     }
 
     private void ToggleWater(bool isOn)
