9c15ab29f4b354dba79bb710978dfae0b36b6653
Master-YEET <christopher.robidas@outlook.com>
2022-05-14 12:14:32 -0400

    Added torch interaction indicator

--- ElemEnsions/Assets/Script/InteractableManager/InteractableManager.cs
+++ ElemEnsions/Assets/Script/InteractableManager/InteractableManager.cs
@@ -7,9 +7,13 @@ public class InteractableManager : MonoBehaviour
     [SerializeField] private GameObject _canvas;
     [SerializeField] private GameObject _indicatorPrefab;
     [SerializeField] private Transform _playerTransform;
+    [SerializeField] private Camera _playerCamera;
     
     //KEY: IInteractable, VALUE: Indicator
     private Dictionary<GameObject, GameObject> _indicatorsByInteractables;
+
+    private const float SHOW_PROXIMITY_INDICATOR_DISTANCE = 5.0f;
+    private const float SHOW_INTERACTION_INDICATOR_DISTANCE = 2.0f;
     
     private void Awake()
     {
@@ -31,34 +35,34 @@ public class InteractableManager : MonoBehaviour
         
         foreach (GameObject interactable in interactables)
         {
-            GameObject handIndicator = Instantiate(_indicatorPrefab, _canvas.transform);
-            _indicatorsByInteractables.Add(interactable, handIndicator);
+            GameObject indicator = Instantiate(_indicatorPrefab, _canvas.transform);
+            _indicatorsByInteractables.Add(interactable, indicator);
         }
     }
 
     private void UpdateIndicators()
     {
-        foreach (var pair in _indicatorsByInteractables)
+        foreach ((GameObject key, GameObject value) in _indicatorsByInteractables)
         {
-            if (pair.Key == null)
+            Vector3 interactableObjectPosition = key.transform.position;
+            Vector3 viewportPosition = _playerCamera.WorldToViewportPoint(interactableObjectPosition);
+            bool showIndicator = Vector3.Distance(_playerTransform.position, interactableObjectPosition) < SHOW_PROXIMITY_INDICATOR_DISTANCE &&
+                                 IsVisibleByPlayerCamera(key);
+
+            if (showIndicator)
             {
-                _indicatorsByInteractables.Remove(pair.Key);
-                return;
+                value.GetComponent<RectTransform>().anchorMax = viewportPosition;
+                value.GetComponent<RectTransform>().anchorMin = viewportPosition;
+                value.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
             }
             
-            Vector3 pos = Camera.main.WorldToViewportPoint(pair.Key.transform.position) - Vector3.up * 0.02f;
-
-            if (Vector3.Distance(_playerTransform.position, pair.Key.transform.position) < 2 && pos.z > 0 && pos.x > 0 && pos.x < 1 && pos.y > 0 && pos.y < 1)
-            {
-                pair.Value.GetComponent<RectTransform>().anchorMax = pos;
-                pair.Value.GetComponent<RectTransform>().anchorMin = pos;
-                pair.Value.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-                pair.Value.SetActive(true);
-            }
-            else
-            {
-                pair.Value.SetActive(false);
-            }
+            value.SetActive(showIndicator);
         }
+    }
+
+    private bool IsVisibleByPlayerCamera(GameObject objectToCheck)
+    {
+        Vector3 viewportPosition = _playerCamera.WorldToViewportPoint(objectToCheck.transform.position);
+        return viewportPosition.z > 0 && viewportPosition.x is > 0 and < 1 && viewportPosition.y is > 0 and < 1;
     }
 }
