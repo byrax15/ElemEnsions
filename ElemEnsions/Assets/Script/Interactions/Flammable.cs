650d93a98e446da261ab0d1011ffa6b3cdf099be
Master-YEET <christopher.robidas@outlook.com>
2022-05-14 17:10:24 -0400

    Burning effect

--- ElemEnsions/Assets/Script/Interactions/Flammable.cs
+++ ElemEnsions/Assets/Script/Interactions/Flammable.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 public class Flammable : Interactable
 {
     [SerializeField] private PlayerController _playerController;
+    [SerializeField] private float _burningDuration = 3.0f;
     
+    private static readonly int progress = Shader.PropertyToID("_Progress");
+
     private const string TORCH_TAG = "Torch";
-    
+
     public override bool Interact()
     {
         return Burn();
@@ -25,7 +28,13 @@ public class Flammable : Interactable
     
     private IEnumerator PlayBurnEffect()
     {
-        yield return new WaitForSeconds(3);
+        for (float time = 0f; time < _burningDuration; time += Time.deltaTime)
+        {
+            GetComponent<Renderer>().material.SetFloat(progress, Mathf.Lerp(1, 0, time / _burningDuration));
+            yield return null;
+        }
+        
+        GetComponent<Renderer>().material.SetFloat(progress, 0);
         Destroy(gameObject);
     }
 }
